6efe159897c0545b795177cf025c92218ecd681c
geft <lifeof843@gmail.com>
2015-02-09 20:57:49 +0700

    Space replaced with _

--- hearthhead/download.py
+++ hearthhead/download.py
@@ -22,7 +22,7 @@ def save(url, dir_name, file_name):
     dir_name = "C:\\Users\\Gerry\\Desktop\\" + dir_name[:-1]
     path = dir_name + "\\" + file_name
     os.makedirs(dir_name, exist_ok=True)
-    url = str.replace(url, " ", "%20")
+    url = str.replace(url, " ", "_")
     urllib.request.urlretrieve(url, path)
 
 
