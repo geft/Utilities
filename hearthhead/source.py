354f5fc9172322f3a0095fb29053a099c47e3eb7
geft <lifeof843@gmail.com>
2015-02-09 20:54:59 +0700

    Fix page not found bug

--- hearthhead/source.py
+++ hearthhead/source.py
@@ -1,6 +1,7 @@
 __author__ = 'Gerry'
 
 import urllib.request
+import urllib.error
 
 
 def is_source_valid(source):
@@ -8,6 +9,12 @@ def is_source_valid(source):
 
 
 def get_source_from_page(page):
-    source = urllib.request.urlopen("http://www.hearthhead.com/card=" + page).read()
+    try:
+        source = urllib.request.urlopen("http://www.hearthhead.com/card=" + page).read()
+    except urllib.error.HTTPError:
+        print("Page " + page + " not found")
+        return ""
+
+
     return source.decode("utf-8")
 
