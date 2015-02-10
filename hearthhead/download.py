d311bcdc804024bddd9483a615ff4591093e364f
geft <lifeof843@gmail.com>
2015-02-10 17:42:53 +0700

    Take "Cast" sound keyword into account

--- hearthhead/download.py
+++ hearthhead/download.py
@@ -47,10 +47,12 @@ def reformat_sound_name(file_name):
         file_end = "Attack"
     elif "_Death" in file_name:
         file_end = "Death"
-    elif "_Trigger" in file_name or "_Other" in file_name:
+    elif "_Trigger" in file_name or "_effect" in file_name:
         file_end = "Trigger"
     elif "_Play" in file_name or "_EnterPlay" in file_name:
         file_end = "Play"
+    elif "_Cast" in file_name:
+        file_end = "Cast"
     else:
         file_end = "Unknown"
 
