771498a2ce5646337c1d17aca6f56215f444a8bd
Gerry <gerry.juans@traveloka.com>
2017-12-09 14:14:45 +0700

    Add comment for setup

--- hearthstone/hearthpwn/run.py
+++ hearthstone/hearthpwn/run.py
@@ -1,6 +1,7 @@
 from directory import check_output_directories
 from downloader import start_download
 
+# Add source code to content.txt along with cards.json to the desktop
 content_path = "C:\\Users\\Gerry\\Desktop\\content.txt"
 
 with open(content_path, 'r') as file:
